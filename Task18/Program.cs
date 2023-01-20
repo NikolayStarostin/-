7c95159435e37bc36221994b2b14ef88217b0acc
Squirrel <5260850@gmail.com>
2023-01-20 21:02:02 +0300

    add Task 18

---
+++ Task18/Program.cs
@@ -0,0 +1,20 @@
+﻿// 18. Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти");
+
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string result = RangeCoords(quarter);
+
+Console.WriteLine(result);
+
+string RangeCoords(int coords)
+{
+    if (coords == 1) return ("x > 0, y > 0");
+    if (coords == 2) return ("x < 0, y > 0");
+    if (coords == 3) return ("x < 0, y < 0");
+    if (coords == 4) return ("x > 0, y < 0");
+    return ("Введены некорректные данные");
+}

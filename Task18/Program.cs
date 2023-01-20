f00e42faf0f20034deadb2709ccbcaed9d15fac9
Squirrel <5260850@gmail.com>
2023-01-20 21:08:37 +0300

    code is optimized

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -4,17 +4,17 @@
 
 Console.WriteLine("Введите номер четверти");
 
-int quarter = Convert.ToInt32(Console.ReadLine());
+string quarter = Console.ReadLine();
 
 string result = RangeCoords(quarter);
 
 Console.WriteLine(result);
 
-string RangeCoords(int coords)
+string RangeCoords(string coords)
 {
-    if (coords == 1) return ("x > 0, y > 0");
-    if (coords == 2) return ("x < 0, y > 0");
-    if (coords == 3) return ("x < 0, y < 0");
-    if (coords == 4) return ("x > 0, y < 0");
+    if (coords == "1") return ("x > 0, y > 0");
+    if (coords == "2") return ("x < 0, y > 0");
+    if (coords == "3") return ("x < 0, y < 0");
+    if (coords == "4") return ("x > 0, y < 0");
     return ("Введены некорректные данные");
 }

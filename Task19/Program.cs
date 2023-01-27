99447790317b21cdce2ca9df959f96a27ac99368
Squirrel <5260850@gmail.com>
2023-01-27 15:06:46 +0300

    add Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,8 +4,20 @@
 // оно палиндромом.
 
 // 14212 -> нет
-
 // 12821 -> да
-
 // 23432 -> да
 //===================================================
+
+
+Console.WriteLine("Введите пятизначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+number = Math.Abs(number);
+
+if (number < 10000 || number > 99999)
+    Console.WriteLine("Некорректный ввод");
+else
+    if (number / 10000 % 10 == number % 10 && number / 1000 % 10 == number / 10 % 10)
+    Console.WriteLine("да");
+    else
+    Console.WriteLine("нет");

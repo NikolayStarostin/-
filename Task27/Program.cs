6201753197b78888267bb2ac06237ce5639eafe1
Squirrel <5260850@gmail.com>
2023-01-30 22:24:54 +0300

    add Task27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,7 +1,37 @@
-﻿// ======================Задача 24================================
-//  Напишите цикл, который принимает на вход два числа (A и B)
-//  и возводит число A в натуральную степень B.
+﻿// ======================Задача 27================================
+// Напишите программу, которая принимает на вход
+// число и выдаёт сумму цифр в числе.
 
-// 3, 5 -> 243 (3⁵)
-// 2, 4 -> 16
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
 //===============================================================
+
+int NumberLen(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+void SumOfNumbers(int n, int len)
+{
+    int sum = 0;
+    int startNumber = n;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"Cумма цифр числа {startNumber} -> {sum}");
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int len = NumberLen(number);
+SumOfNumbers(number, len);

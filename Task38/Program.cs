0b622ad1e8830b67860822d61fea3d5924ee89b5
Squirrel <5260850@gmail.com>
2023-02-03 12:52:18 +0300

    add Task38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -7,27 +7,73 @@
 // ==============================================================
 
 
-
-
-double[] CreateArrayRndDouble (int size, int min, int max)
+double[] CreateArrayRndDouble(int size, int min, int max)
 {
     double[] arr = new double[size];
     Random rnd = new Random();
     for (int i = 0; i < arr.Length; i++)
     {
         double num = rnd.NextDouble() * (max - min) + min;
+        //arr[i] = Convert.ToDouble(rnd.Next(min, max));
         arr[i] = Math.Round(num, 1);
     }
     return arr;
 }
 
-void PrintArrayDouble (double[] arr)
+void PrintArrayDouble(double[] arr)
 {
     Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
-        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
         else Console.Write($"{arr[i]}");
     }
     Console.Write("]");
 }
+
+double FindMaxDigit(double[] arr)
+{
+    {
+        double max = 0;
+        for (int j = 0; j < arr.Length; j++)
+        {
+            if (arr[j] > max)
+                max = arr[j];
+        }
+        max = Math.Round(max, 1);
+        return max;
+    }
+}
+
+double FindMinDigit(double[] arr)
+{
+    {
+        double min = arr[0];
+        for (int k = 0; k < arr.Length; k++)
+        {
+            if (arr[k] < min)
+                min = arr[k];
+        }
+        min = Math.Round(min, 1);
+        return min;
+    }
+}
+
+double Difference(double mindigit, double maxdigit)
+{
+    double diff = 0;
+    {
+        diff = maxdigit - mindigit;
+        diff = Math.Round(diff, 1);
+    }
+    return diff;
+}
+
+Console.WriteLine("Задайте размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] arr = CreateArrayRndDouble(size, 1, 99);
+PrintArrayDouble(arr);
+double findmax = FindMaxDigit(arr);
+double findmin = FindMinDigit(arr);
+double difference = Difference(findmin, findmax);
+Console.Write($" -> {difference}");

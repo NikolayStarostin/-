73f3e77b9cd4f9c656869a3ed49460ceb523fc3f
Squirrel <5260850@gmail.com>
2023-02-07 06:22:35 +0300

    add Task43

---
+++ Task41/Program.cs
@@ -0,0 +1,35 @@
+﻿// =====================Задача 41================================
+// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+// ==============================================================
+
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+
+Console.WriteLine("Задайте размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArray(size, -1000, 1000);
---
+++ Task43/Program.cs
@@ -0,0 +1,47 @@
+﻿// =====================Задача 43================================
+// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// ==============================================================
+
+// Формула нахождения кординаты точки:
+// x = (b2 - b1) / (k1 - k2)
+// y = k1 * (b2 - b1) / (k1 - k2) + b1
+
+
+double ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double FindX(double findxb1, double findxb2, double findxk1, double findxk2)
+{
+    double coordx;
+    coordx = (findxb1 - findxb2) / (findxk2 - findxk1);
+    coordx = Math.Round(coordx, 1);
+    return coordx;
+}
+
+double FindY(double findyb1, double findyb2, double findyk1, double findyk2)
+{
+    double coordy;
+    return coordy = Math.Round((findyk1 * (findyb2 - findyb1) / (findyk1 - findyk2) + findyb1), 1);
+}
+
+double b1 = ReadInt("Введите значение b1: ");
+double k1 = ReadInt("Введите значение k1: ");
+double b2 = ReadInt("Введите значение b2: ");
+double k2 = ReadInt("Введите значение k2: ");
+double x = FindX(b1, b2, k1, k2);
+double y = FindY(b1, b2, k1, k2);
+if (b1 == b2 ^ k1 == k2)
+    Console.WriteLine("Прямые не пересекаются");
+if (b1 == b2 & k1 == k2)
+    Console.WriteLine("Прямые совпадают");
+if (b1 != b2 & k1 != k2)
+    Console.WriteLine($"({x}:{y})");
+
+
+

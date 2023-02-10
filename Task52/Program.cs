816b3b0b41a138ba67922ce519004bc2b5fa43a7
Squirrel <5260850@gmail.com>
2023-02-10 12:29:14 +0300

    add Task52

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// =====================Задача 52================================
+// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+//   1  4  7  2
+//   5  9  2  3
+//   8  4  2  4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+// ==============================================================
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}  | ");
+            else Console.Write($"{matrix[i, j],3}  ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void AverageSum(int[,] matrix)
+{
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            sum += matrix[i, j];
+        if (j < matrix.GetLength(1) - 1) Console.Write($"{Math.Round(sum / matrix.GetLength(0), 1)};  ");
+        else Console.Write($"{Math.Round(sum / matrix.GetLength(0), 1)}.");
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+Console.WriteLine();
+AverageSum(array2D);
+
+
+
